34a642e1065ca9bf37d88ac0e0542ab3474fe9fe
Gabr1el94 <bielsystem64@gmail.com>
2023-03-05 18:28:58 -0300

    prompts questions elaborated

---
+++ bitcoin-legal-api/src/db/prompts.ts
@@ -0,0 +1,17 @@
+export const questions = [
+    {
+        question: "displays in json, only country names and the statuses that accept or banned bitcoin in Latin America"
+    },
+    {
+        question: "displays in json, only country names and the statuses that accept or banned bitcoin in North America"
+    },
+    {
+        question: "displays in json, only country names and the statuses that accept or banned bitcoin in Europe"
+    },
+    {
+        question: "displays in json, only country names and the statuses that accept or banned bitcoin in Africa"
+    },
+    {
+        question: "displays in json, only country names and statuses that accept or banned bitcoin in Asia"
+    }
+]
